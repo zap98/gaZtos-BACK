cb93ba51929bc5a6567750769fb74b320be19637
Ana Diaz Muñoz <admcr00@gmail.com>
2024-08-08 23:15:03 +0200

    Subida de la parte Back

---
+++ gAZtos/gAZtos/src/main/java/com/example/gAZtos/GAZtosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gAZtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GAZtosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GAZtosApplication.class, args);
+	}
+
+}
---
+++ gAZtos/gAZtos/src/test/java/com/example/gAZtos/GAZtosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gAZtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GAZtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
